cc1295038727753fa7b1d28c4ae68591b6134c0f
Gejun <14931038+Gejun_214@user.noreply.gitee.com>
2026-01-04 00:58:41 +0800

    添加复现流程，修复数据集加载

--- 5，医学图像分割/Unet/test.py
+++
@@ -1,7 +0,0 @@
-
-import torch, torchvision, numpy, PIL
-print("torch:", torch.__version__)
-print("torchvision:", torchvision.__version__)
-print("numpy:", numpy.__version__)
-print("cuda:", torch.cuda.is_available())
-
